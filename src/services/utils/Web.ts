b62d65e521e2dc9fa5fb2de638c2d45290de3b02
IMJI <imjustice@yandex.ru>
2022-07-26 20:02:51 +0300

    Added sort class

--- src/services/utils/Web.ts
+++ src/services/utils/Web.ts
@@ -3,9 +3,28 @@ enum SortingOrders {
     Descending
 }
 
-type Sort = {
-    column : string;
-    order : SortingOrders;
+class Sort {
+    private column : string;
+    public get Column() : string { return this.column; }
+    private order : SortingOrders;
+    public get Order() : SortingOrders { return this.order; }
+
+    constructor(column : string, order : SortingOrders) {
+        this.column = column;
+        this.order = order;
+    }
+
+    public static FromString(str : string) : Sort[] {
+        const sortings : Sort[] = [];
+        const sortingsStr : string[] = str.split(',');
+        sortingsStr.forEach(sorting => {
+            const opts = sorting.split(':');
+            let order : SortingOrders = opts[1].toLowerCase() === 'desc' ? SortingOrders.Descending : SortingOrders.Ascending;
+            const s : Sort = new Sort(opts[0], order);
+            sortings.push(s);
+        });
+        return sortings;
+    }
 }
 
 interface Query {
