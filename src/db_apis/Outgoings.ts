8cf7a08446a2020860951f129c8a56c5ca6342c5
IMJI <imjustice@yandex.ru>
2022-07-27 23:37:41 +0300

    Disconnected Outgoings API

--- src/db_apis/Outgoings.ts
+++ src/db_apis/Outgoings.ts
@@ -25,61 +25,63 @@ class Outgoings {
     `;
 
     public static async Find(context : OutgoingQuery) : Promise<OutgoingModel[]> {
-        let query : string = this.findQuery;
-        let binds : oracledb.BindParameters = {};
+        // let query : string = this.findQuery;
+        // let binds : oracledb.BindParameters = {};
 
-        if (context.id) {
-            binds.out_id = context.id;
-            query += `\nand o.out_id = :out_id`;
-        }
-        if (context.vendor_code) {
-            binds.vendor_code = QueryStringToSQLList(context.vendor_code);
-            query += `\nand o.vendor_code in (:vendor_code)`;
-        }
-
-        if (context.manager) {
-            binds.manager = context.manager;
-            query += `\nand o.man_id in (${context.manager})`;
-        }
-        if (context.category) {
-            //binds.category = QueryStringToSQLList(context.category);
-            let categories : string = QueryStringToSQLList(context.category);
-            //console.log(QueryStringToSQLList(context.category));
-            query += `\nand c.cat_name in (${categories})`;
-        }
-        // If from or to will be empty?
-        if (context.quantity) {
-            binds.quantity_from = context.quantity.split(':')[0];
-            binds.quantity_to = context.quantity.split(':')[1];
-            query += `\nand (o.quantity >= :quantity_from and o.quantity <= :quantity_to)`;
-        }
-        if (context.cost) {
-            binds.cost_from = context.cost.split(':')[0];
-            binds.cost_to = context.cost.split(':')[1];
-            query += `\nand (o.cost >= :cost_from and o.cost <= :cost_to)`;
-        }
-        // if (context.sort === undefined && context.sort !== []) {
-        //     query += '\norder by o.out_id asc';
-        // } else {
-        //     query += '\norder by ';
-        //     for (let i = 0; i < context.sort.length; i++) {
-        //         const sorting : Sort = context.sort[i];
-        //         if (i > 0) query += ', ';
-        //         query += `${sorting.Column} ${sorting.Order}`;
-        //     }
-        //     /* if (!sortableColumns.includes(column)) {
-        //         throw new Error('Invalid "sort" column');
-        //     } */            
+        // if (context.id) {
+        //     binds.out_id = context.id;
+        //     query += `\nand o.out_id = :out_id`;
         // }
-        if (context.skip) {
-            binds.row_offset = context.skip;
-            query += '\noffset :row_offset rows';
-        }
-        let limit = (+context.limit > 0) ? context.limit : 20;
-        binds.row_limit = limit;
-        query += '\nfetch next :row_limit rows only';
-        const result : oracledb.Result<OutgoingModel> = await Database.Execute(query, binds);
-        return result.rows;
+        // if (context.vendor_code) {
+        //     binds.vendor_code = QueryStringToSQLList(context.vendor_code);
+        //     query += `\nand o.vendor_code in (:vendor_code)`;
+        // }
+
+        // if (context.manager) {
+        //     binds.manager = context.manager;
+        //     query += `\nand o.man_id in (${context.manager})`;
+        // }
+        // if (context.category) {
+        //     //binds.category = QueryStringToSQLList(context.category);
+        //     let categories : string = QueryStringToSQLList(context.category);
+        //     //console.log(QueryStringToSQLList(context.category));
+        //     query += `\nand c.cat_name in (${categories})`;
+        // }
+        // // If from or to will be empty?
+        // if (context.quantity) {
+        //     binds.quantity_from = context.quantity.split(':')[0];
+        //     binds.quantity_to = context.quantity.split(':')[1];
+        //     query += `\nand (o.quantity >= :quantity_from and o.quantity <= :quantity_to)`;
+        // }
+        // if (context.cost) {
+        //     binds.cost_from = context.cost.split(':')[0];
+        //     binds.cost_to = context.cost.split(':')[1];
+        //     query += `\nand (o.cost >= :cost_from and o.cost <= :cost_to)`;
+        // }
+        // // if (context.sort === undefined && context.sort !== []) {
+        // //     query += '\norder by o.out_id asc';
+        // // } else {
+        // //     query += '\norder by ';
+        // //     for (let i = 0; i < context.sort.length; i++) {
+        // //         const sorting : Sort = context.sort[i];
+        // //         if (i > 0) query += ', ';
+        // //         query += `${sorting.Column} ${sorting.Order}`;
+        // //     }
+        // //     /* if (!sortableColumns.includes(column)) {
+        // //         throw new Error('Invalid "sort" column');
+        // //     } */            
+        // // }
+        // if (context.skip) {
+        //     binds.row_offset = context.skip;
+        //     query += '\noffset :row_offset rows';
+        // }
+        // let limit = (+context.limit > 0) ? context.limit : 20;
+        // binds.row_limit = limit;
+        // query += '\nfetch next :row_limit rows only';
+        // const result : oracledb.Result<OutgoingModel> = await Database.Execute(query, binds);
+        // return result.rows;
+        let x : OutgoingModel[] = [];
+        return x;
     }
 
     private static countQuery : string = `
@@ -94,11 +96,12 @@ class Outgoings {
     `;
 
     public static async Count(context : OutgoingQuery) : Promise<number> {
-        let query : string = this.countQuery;
-        let binds : oracledb.BindParameters = {};
+        // let query : string = this.countQuery;
+        // let binds : oracledb.BindParameters = {};
 
-        const result : oracledb.Result<number> = await Database.Execute(query, binds);
-        return result.rows[0]['ROWS_COUNT'];
+        // const result : oracledb.Result<number> = await Database.Execute(query, binds);
+        // return result.rows[0]['ROWS_COUNT'];
+        return -1;
     }
 }
 
