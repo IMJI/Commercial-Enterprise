04b6281a6a2d61b80bf3d198c81660f8ef3c99ae
IMJI <imjustice@yandex.ru>
2022-08-12 17:54:51 +0300

    Database module fixes

--- src/services/Database.ts
+++ src/services/Database.ts
@@ -6,45 +6,6 @@ import { resolve } from 'path';
 import Models from '../models/Models';
 
 class Database {
-    private static pool : oracledb.Pool;
-    
-    public static async OldInitialize() : Promise<void> {
-        //this.pool = await oracledb.createPool(Config.Database.dbPool);
-        if (this.pool) Logger.Info('Database initialized');
-        else Logger.Fatal('Database initialization error: Cannot craete pool');
-    }
-
-    public static async OldExecute(statement : string, binds : oracledb.BindParameters = [], opts : oracledb.ExecuteOptions = {}) {
-        return new Promise(async (resolve, reject) => {
-            let connection : oracledb.Connection;
-            opts.outFormat = oracledb.OUT_FORMAT_OBJECT;
-            opts.autoCommit = true;
-            try {
-                connection = await this.OldConnect();
-                let result : oracledb.Result<object> = await connection.execute(statement, binds, opts);
-                resolve(result);
-            } catch(error : any) {
-                reject(error);
-            } finally {
-                if (connection) {
-                    try { await connection.close(); }
-                    catch(error : any) {
-                        Logger.Error(error.message || error);
-                    }
-                }
-            }
-        });
-    }
-
-    public static async OldClose() : Promise<void> {
-        await this.pool.close();
-        Logger.Info('Database closed');
-    }
-
-    private static async OldConnect() : Promise<oracledb.Connection> {
-        return await oracledb.getConnection();
-    }
-
     private static dataSource : DataSource;
 
     public static async Initialize() : Promise<void> {
@@ -55,22 +16,25 @@ class Database {
             username: Config.Database.username,
             password: Config.Database.password,
             database: Config.Database.database,
-            synchronize: false,
+            synchronize: true,
             logging: true,
             entities: Models,
             subscribers: [],
             migrations: [],
+            extra: {
+                autoCommit: true
+            }
         });
 
-        await this.dataSource.initialize().then(() => {
-            Logger.Info('Database module started')
+        await this.dataSource.initialize().then(async () => {
+            Logger.Info('Database module started');
             resolve();
         }).catch((error) => Logger.Error(error))
     }
 
-    public static async Execute() : Promise<void> {
+    // public static async Execute() : Promise<void> {
         
-    }
+    // }
 
     public static async Close() : Promise<void> {
         await this.dataSource.destroy().then(() => {
