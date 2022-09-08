f462e7c71a3c33e353b0b2a1c2b4481564ca968b
IMJI <imjustice@yandex.ru>
2022-09-08 21:36:36 +0300

    Added exception class

---
+++ src/exception/Exception.ts
@@ -0,0 +1,21 @@
+abstract class Exception extends Error {
+    private _isFatal: boolean;
+    public get isFatal(): boolean {
+        return this._isFatal;
+    }
+
+    private _httpCode?: number;
+    public get httpCode(): number {
+        if (this._httpCode) return this._httpCode;
+        else return -1;
+    }
+
+    constructor(msg: string, isFatal: boolean = false, httpCode?: number) {
+        super(msg);
+        Object.setPrototypeOf(this, Exception.prototype);
+        this._isFatal = isFatal;
+        if (httpCode) this._httpCode = httpCode;
+    }
+}
+
+export default Exception;
