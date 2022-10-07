5aeacb7138151d916f8d6b620f0ee4ac7ba72dfa
IMJI <imjustice@yandex.ru>
2022-10-07 17:57:26 +0300

    Implemented new validation

--- src/routes/ApiRouter.ts
+++ src/routes/ApiRouter.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import TaxController from '../controllers/api/taxes/TaxController';
+import ValidationMiddleware from '../middlewares/Validation';
+import { taxQueryScheme } from '../models/tax/utils/TaxUtilities';
 
 const apiRouter: Router = Router();
 const apiPath = '/api';
 
-apiRouter.route(`${apiPath}/taxes/:id?`).get(TaxController.get);
+const taxValidation = ValidationMiddleware.validate(taxQueryScheme, taxQueryScheme);
+
+apiRouter.route(`${apiPath}/taxes/:id?`).get(taxValidation, TaxController.get);
 
 export default apiRouter;
