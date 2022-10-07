853496fe13c0334c9e0b0008538ed03b1d603083
IMJI <imjustice@yandex.ru>
2022-10-07 16:55:52 +0300

    Added utility types

---
+++ src/services/utils/Schemes.ts
@@ -0,0 +1,20 @@
+import Joi = require('joi');
+
+class Schemes {
+	public static id = Joi.number().integer().positive();
+	public static shortString = Joi.string().min(2).max(64);
+	public static oneOrMoreShortString = [
+		this.shortString,
+		Joi.array().items(this.shortString)
+	];
+	public static bool = Joi.boolean();
+	public static money = Joi.number().positive().precision(2);
+	public static positiveInt = Joi.number().integer().min(0);
+	public static query = Joi.object({
+		limit: Schemes.positiveInt,
+		skip: Schemes.positiveInt,
+		sort: Joi.string().pattern(new RegExp('[a-zA-Z]+:(ASC|DESC|asc|desc)$'))
+	});
+}
+
+export default Schemes;
