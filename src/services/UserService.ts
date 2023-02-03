86b480eb81989bfceecb188e0b1d825bd692e552
IMJI <imjustice@yandex.ru>
2023-02-03 15:36:49 +0300

    Added find by email function

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -15,37 +15,27 @@ class UserService implements IService<User> {
 		const result = await User.findOneBy({ id });
 		return result;
 	}
+
+	public async findOneByEmail(email: string): Promise<User> {
+		const result = await User.findOneBy({ email });
+		return result;
+	}
+
 	public async find(options: FindOptions): Promise<User[]> {
 		throw new Error('Method not implemented.');
 	}
+
 	public async count(options: FindOptions): Promise<number> {
 		throw new Error('Method not implemented.');
 	}
+
 	public async findAndCount(
 		options: FindOptions
 	): Promise<ReadAndCountResult<User>> {
 		throw new Error('Method not implemented.');
 	}
+
 	public async create(dto: UserDTO): Promise<User> {
-		// const { manager, role, password } = dto;
-		// const managerObject = await ManagerReader.readOne(manager);
-		// if (!managerObject) throw new NotFoundException(`Can't find manager with id = ${manager}`);
-		// const salt = await bcrypt.genSalt(10);
-		// const hash = await bcrypt.hash(password, salt);
-		// const user = User.create({
-		// 	manager: managerObject,
-		//     role,
-		//     password: hash
-		// });
-		// await User.save(user);
-
-		// return user;
-
-		// Get data from dto
-		// Create Manager entity
-		// Generate hash
-		// Create UserPassword entity
-		// Create User entity
 		const salt = await bcrypt.genSalt(10);
 		const hash = await bcrypt.hash(dto.password, salt);
 		dto.password = hash;
@@ -56,9 +46,11 @@ class UserService implements IService<User> {
 
 		return user;
 	}
+
 	public async update(dto: object): Promise<User> {
 		throw new Error('Method not implemented.');
 	}
+
 	public async delete(id: number): Promise<DeleteResult> {
 		throw new Error('Method not implemented.');
 	}
