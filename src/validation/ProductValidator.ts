fd187f6c977ca1857734f6513248c5ea5eb53067
IMJI <imjustice@yandex.ru>
2023-02-03 15:11:26 +0300

    Added product validation

---
+++ src/validation/ProductValidator.ts
@@ -0,0 +1,27 @@
+import Joi = require('joi');
+import Schemas from './Schemas';
+
+const productQuerySchema = Schemas.query.append({
+	name: Schemas.oneOrMoreShortString,
+	category: Schemas.oneOrMoreShortString,
+	description: Joi.string(),
+	isDeleted: Schemas.bool
+});
+
+const productBodySchema = Schemas.query.append({
+	vendorCode: Schemas.id,
+	name: Schemas.shortString,
+	category: Schemas.id,
+	description: Schemas.text,
+	isDeleted: Schemas.bool
+});
+
+const productBodyStrictSchema = Schemas.query.append({
+	vendorCode: Schemas.id.required(),
+	name: Schemas.shortString.required(),
+	category: Schemas.id.required(),
+	description: Schemas.text,
+	isDeleted: Schemas.bool
+});
+
+export { productQuerySchema, productBodySchema, productBodyStrictSchema };
