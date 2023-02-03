04b0831e5463cc21776d2d2e2f6dd318cd035ed3
IMJI <imjustice@yandex.ru>
2023-02-03 18:10:25 +0300

    Added login router

--- src/routes/WebRouter.ts
+++ src/routes/WebRouter.ts
@@ -3,8 +3,11 @@ import LoginController from '../controllers/auth/LoginController';
 import IndexController from '../controllers/IndexController';
 
 const webRouter: Router = Router();
+const loginController = new LoginController('login');
 
 webRouter.get('/', IndexController.get);
-webRouter.get('/login', LoginController.get);
+webRouter
+    .get('/login', loginController.get)
+    .post('/login', loginController.post);
 
 export default webRouter;
