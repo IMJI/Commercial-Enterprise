b5c67d17ae5f8687884583fe414b88cead3ddb84
IMJI <imjustice@yandex.ru>
2023-02-06 13:59:13 +0300

    Added DTO for price

---
+++ src/models/price/PriceDTO.ts
@@ -0,0 +1,8 @@
+class PriceDTO {
+    productId: number;
+    dateFrom: Date;
+    dateTo: Date;
+    value: number;
+}
+
+export default PriceDTO;
---
+++ src/models/price/PriceMapper.ts
@@ -0,0 +1,24 @@
+import Price from "./Price";
+import PriceDTO from "./PriceDTO";
+
+class PriceMapper {
+    public static toDomain(dto: PriceDTO): Price {
+        const price = new Price();
+        if (dto.productId) price.productId = dto.productId;
+        if (dto.dateFrom) price.dateFrom = dto.dateFrom;
+        if (dto.dateTo) price.dateTo = dto.dateTo;
+        if (dto.value) price.value = dto.value;
+
+        return price;
+    }
+
+    public static toDTO(domain: Price): PriceDTO {
+        const dto = new PriceDTO();
+        dto.productId = domain.productId;
+        dto.dateFrom = domain.dateFrom;
+        dto.dateTo = domain.dateTo;
+        dto.value = domain.value;
+
+        return dto;
+    }
+}
