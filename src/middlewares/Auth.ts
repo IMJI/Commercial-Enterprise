77caeecff8cf23d014a0953bd1a6ed11c2e685b2
IMJI <imjustice@yandex.ru>
2023-02-18 22:51:12 +0300

    Refactoring

--- src/middlewares/Auth.ts
+++ src/middlewares/Auth.ts
@@ -7,51 +7,47 @@ import Config from '../Config';
 import Roles from '../types/utils/Roles';
 
 class Auth {
-    private role: Roles; 
+	private role: Roles;
 
-    constructor(role = Roles.any) {
-        this.role = role;
-    }
+	constructor(role = Roles.any) {
+		this.role = role;
+	}
 
-	public auth(
-        req: Request,
-		res: Response,
-		next: NextFunction
-    ) {
-        const token = this.getToken(req);
-        const payload = this.checkToken(token);
-        if (this.role === Roles.any || this.role === payload.role) {
-            req['user'] = {
-                id: payload.id,
-                role: payload.role
-            };
-        } else {
-            throw new AuthException('You are not authorized')
-        }
+	public auth(req: Request, res: Response, next: NextFunction) {
+		const token = this.getToken(req);
+		const payload = this.checkToken(token);
+		if (this.role === Roles.any || this.role === payload.role) {
+			req['user'] = {
+				id: payload.id,
+				role: payload.role
+			};
+		} else {
+			throw new AuthException('You are not authorized');
+		}
 
-        next();
-    }
+		next();
+	}
 
-    private getToken(req: Request): string {
-        const authHeader = req.headers.authorization;
-        if (authHeader) {
-            const token = authHeader.split(' ')[1];
-            return token;
-        }
-        throw new AuthException('Auth token is required');
-    }
+	private getToken(req: Request): string {
+		const authHeader = req.headers.authorization;
+		if (authHeader) {
+			const token = authHeader.split(' ')[1];
+			return token;
+		}
+		throw new AuthException('Auth token is required');
+	}
 
-    private checkToken(token: string): Payload {
-        try {
-            const payload = <Payload>jwt.verify(token, Config.database.secretKey);
-            return payload;
-        } catch (error) {
-            if (error.name === 'TokenExpiredError') {
-                throw new AuthException('Token expired');
-            }
-            throw new AuthException('Authentification failed');
-        }
-    }
+	private checkToken(token: string): Payload {
+		try {
+			const payload = <Payload>jwt.verify(token, Config.database.secretKey);
+			return payload;
+		} catch (error) {
+			if (error.name === 'TokenExpiredError') {
+				throw new AuthException('Token expired');
+			}
+			throw new AuthException('Authentification failed');
+		}
+	}
 }
 
 export default Auth;
