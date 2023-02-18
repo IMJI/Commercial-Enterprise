b2b9e23a209ea667222f2631cbfea4338450c0bd
IMJI <imjustice@yandex.ru>
2023-02-18 22:51:27 +0300

    Bug fix

--- src/controllers/auth/LoginController.ts
+++ src/controllers/auth/LoginController.ts
@@ -25,6 +25,7 @@ class LoginController extends Controller {
 		const { email, password } = req.body;
 		try {
 			const user = await UserService.findOneByEmail(email);
+			if (!user) throw new AuthException('Invalid email or password');
 			const isPasswordMatches = await bcrypt.compare(
 				password,
 				user.password.hash
