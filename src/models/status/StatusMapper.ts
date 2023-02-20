9d5bd4ad4ac46601a6e8c5ee66fefaf5c90afbb7
IMJI <imjustice@yandex.ru>
2023-02-20 23:26:56 +0300

    Status update and cancel methods

--- src/models/status/StatusMapper.ts
+++ src/models/status/StatusMapper.ts
@@ -4,8 +4,12 @@ import StatusDTO from "./StatusDTO";
 class StatusMapper {
     public static toDomain(dto: StatusDTO): Status {
         const status = new Status();
-        if (dto.outgoing) status.outgoing = dto.outgoing;
+        if (dto.outgoing) {
+            status.outgoing = dto.outgoing;
+            status.outgoingId = dto.outgoing.id;
+        }
         if (dto.dateFrom) status.dateFrom = dto.dateFrom;
+        else status.dateFrom = new Date();
         if (dto.dateTo) status.dateTo = dto.dateTo;
         if (dto.status) status.status = dto.status;
 
--- src/services/StatusService.ts
+++ src/services/StatusService.ts
@@ -1,4 +1,7 @@
 import { IsNull, LessThanOrEqual, MoreThan } from "typeorm";
+import EntityUpdatingException from "../exceptions/EntityUpdatingException";
+import Exception from "../exceptions/Exception";
+import NotFoundException from "../exceptions/NotFoundException";
 import { Outgoing } from "../models/Models";
 import Status from "../models/status/Status";
 import StatusDTO from "../models/status/StatusDTO";
@@ -49,16 +52,33 @@ class StatusService {
         return status;
     }
 
-    public async update(outgoingId: number): Promise<Status> {
-        throw new Error('Not inmplemented');
+    public async update(dto: StatusDTO): Promise<Status> {
+        const now = new Date();
+        const previous = await this.closePrevious(dto.outgoing.id, now);
+        if (!previous.dateTo) throw new EntityUpdatingException(`Can't close latest status of outgoing with id = ${dto.outgoing.id}`);
+        const status = StatusMapper.toDomain({ outgoing: dto.outgoing, dateFrom: now, status: dto.status });
+        await Status.save(status);
+
+        return status;
     }
 
-    public async cancel(outgoingId: number): Promise<Status> {
-        throw new Error('Not inmplemented');
+    public async cancel(outgoing: Outgoing): Promise<Status> {
+        const now = new Date();
+        const previous = await this.closePrevious(outgoing.id, now);
+        if (!previous.dateTo) throw new EntityUpdatingException(`Can't close latest status of outgoing with id = ${outgoing.id}`)
+        const status = StatusMapper.toDomain({ outgoing, dateFrom: now, status: 'Canceled' });
+        await Status.save(status);
+
+        return status;
     }
 
-    private async closePrevious(outgoingId: number): Promise<Status> {
-        throw new Error('Not inmplemented');
+    private async closePrevious(outgoingId: number, date: Date): Promise<Status> {
+        const previous = await this.findLatest(outgoingId);
+        if (!previous) throw new NotFoundException(`Can't find latest sattus for outgoing with id = ${outgoingId}`);
+        previous.dateTo = date;
+        await Status.save(previous);
+        
+        return previous;
     }
 }
 
