5f81137c0859af9eeee038e7ea42aa6d797e7751
IMJI <imjustice@yandex.ru>
2023-02-20 15:55:05 +0300

    Added mapper for status entity

---
+++ src/models/status/StatusMapper.ts
@@ -0,0 +1,14 @@
+import Status from "./Status";
+import StatusDTO from "./StatusDTO";
+
+class StatusMapper {
+    public static toDomain(dto: StatusDTO): Status {
+        const status = new Status();
+        if (dto.outgoing) status.outgoing = dto.outgoing;
+        if (dto.dateFrom) status.dateFrom = dto.dateFrom;
+        if (dto.dateTo) status.dateTo = dto.dateTo;
+        if (dto.status) status.status = dto.status;
+
+        return status;
+    }
+}
